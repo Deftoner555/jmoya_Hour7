9bcc5862736a10d3963fc8af1914611e1c41942f
Deftoner555 <jmoya555@gmail.com>
2023-03-09 11:37:05 -0800

    added days of month for Dec.

--- Assets/Scripts/BirthdayScript.cs
+++ Assets/Scripts/BirthdayScript.cs
@@ -4,14 +4,39 @@ using UnityEngine;
 
 public class BirthdayScript : MonoBehaviour
 {
-    int x = 1;
-
+    
     void Start()
     {
-        while(x <= 12)
+        int x = 1;
+        while (x <= 12)
         {
             Debug.Log(x);
             x++;
+
+            if (x == 12)
+            {
+                Debug.Log("It's my birthday month!");
+            }
+
+            if (x == 12)
+            {
+                int d = 1;
+                while (d <= 31)
+                {
+                    Debug.Log(d);
+                    d++;
+
+                    if(d == 6)
+                    {
+                        Debug.Log("It's my birthday!");
+                    }
+
+                }
+
+            }
+
         }
+        
     }
+
 }
